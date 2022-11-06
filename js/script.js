7e4584fab3d550fa65db25d68f38a1a56126db9f
Yancod <yanchanceller@gmail.com>
2022-11-06 11:48:42 -0300

    Criacao da funcao que pega o elemento que esta carregado no elemento dom

--- js/script.js
+++ js/script.js
@@ -41,3 +41,16 @@ const img = document.getElementById("person-img");
 const author = document.getElementById("author");
 const job = document.getElementById("trabalho");
 const info = document.getElementById("info");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+let currentItem = 0;
+
+window.addEventListener("DOMContentLoaded", function () {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+  });
